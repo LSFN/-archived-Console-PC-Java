136969dfab8311d300347ba238dee6d4bfccec06
LukeusMaximus <github@lukemurray.co.uk>
2013-07-07 15:30:58 +0100

    Added a display class.

---
+++ src/org/lsfn/console_pc/PilotingDisplay.java
@@ -0,0 +1,14 @@
+package org.lsfn.console_pc;
+
+import java.awt.Frame;
+
+public class PilotingDisplay extends Frame {
+    
+    private StarshipConnection starshipConnection;
+    
+    public PilotingDisplay(StarshipConnection starshipConnection) {
+        this.starshipConnection = starshipConnection;
+    }
+    
+    
+}
